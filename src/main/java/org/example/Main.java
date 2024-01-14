366e43f541eb4a7ef0a399133b853c4dfb77f322
hymavathiyedupati <154816626+hymavathiyedupati@users.noreply.github.com>
2024-01-14 22:11:34 +0000

    Update Main.java
    i have committed the changes..now its working fine

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.printttln("I Added Text Here");
+        System.out.println("I Added Text Here");
     }
 }
